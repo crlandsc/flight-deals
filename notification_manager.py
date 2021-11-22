b16af2b1bae0ba2f38c36142ec3bd8a640c17e33
crlandsc <crlandschoot@gmail.com>
2021-11-22 15:42:33 -0600

    ENVIRONMENT VARIABLE UPDATE

--- notification_manager.py
+++ notification_manager.py
@@ -3,6 +3,11 @@ from twilio.rest import Client
 
 TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+FROM_PHONE_NUMBER = os.environ.get("FROM_HONE_NUMBER")
+TO_PHONE_NUMBER = os.environ.get("TO_PHONE_NUMBER")
+SENDER_EMAIL = os.environ.get("SENDER_EMAIL")
+SENDER_PASSWORD = os.environ.get("SENDER_PASSWORD")
+
 
 class NotificationManager:
     # This class is responsible for sending notifications with the deal flight details.
@@ -13,8 +18,8 @@ class NotificationManager:
     def text_to_phone(self, message):
         message = self.client.messages.create(
             body=message,
-            from_='+13187088180',
-            to='+13157199637'
+            from_=FROM_PHONE_NUMBER,
+            to=TO_PHONE_NUMBER
         )
         # Prints if successfully sent.
         # print(message.sid)
@@ -22,8 +27,8 @@ class NotificationManager:
     def send_emails(self, emails, message, google_flight_link):
         with smtplib.SMTP("smtp.gmail.com", 587) as connection:
             connection.starttls()
-            sender_email = "pythoncodetest20@gmail.com"
-            sender_password = "h9n%g!)FF"
+            sender_email = SENDER_EMAIL
+            sender_password = SENDER_PASSWORD
             connection.login(sender_email, sender_password)
             for email in emails:
                 connection.sendmail(
